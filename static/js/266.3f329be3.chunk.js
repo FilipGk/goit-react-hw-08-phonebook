a67601d41dcf3c88b95534493d0a22889a54ed0e
FilipGk <113462544+FilipGk@users.noreply.github.com>
2023-07-16 08:29:18 +0000

    Deploying to gh-pages from @ FilipGk/goit-react-hw-08-phonebook@0ad3606460f108d6829bb456c620c62178102440 🚀

---
+++ static/js/266.3f329be3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(3329);function o(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{style:{textAlign:"center",fontSize:"36px",marginTop:"30px",width:"350px",marginLeft:"auto",marginRight:"auto"},children:"Welcome in your phonebook!"})})}}}]);
+//# sourceMappingURL=266.3f329be3.chunk.js.map
